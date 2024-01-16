c0966e74dac85c2e66f4e125384fd032ea3baf14
Piyush Kumar Singh <142776115+strawhat-90@users.noreply.github.com>
2024-01-16 22:59:00 +0530

    Add files via upload

---
+++ 16_FunRecursion.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int factorial(int n)
+{
+    if (n == 1)
+    {
+        return 1;
+    }
+    return n * factorial(n - 1);
+}
+
+int main()
+{
+    int num;
+
+    cout << "Enter the no. : ";
+    cin >> num;
+
+    cout << factorial(num);
+}
